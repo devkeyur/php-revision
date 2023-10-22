02b5b9462d0f2d88f16bcf77341967d20a89c550
keyur malete <keyurmalete1554@gmail.com>
2023-10-22 09:41:15 +0530

    update

---
+++ 06_operators.php
@@ -0,0 +1,32 @@
+<?php
+    // operators are to use perform some operation between value or variable
+    // arithmetic op
+
+    $x = 10;
+    $y = 10;
+    
+    
+    echo "addition = > ". $x + $y ."<br />";
+    echo "subtraction = > ". $x - $y ."<br />";
+    echo "multiplication = > ". $x * $y ."<br />";
+    echo "divide = > ". $x / $y ."<br />";
+    echo "module = > ". $x % $y ."<br />";
+    echo "pre inc = > ". ++$x ."<br />";
+    echo "pre dec = > ". --$x ."<br />";
+    echo "post inc = > ". $x++ ."<br />";
+    echo "post dec = > ". $x-- ."<br />";
+
+    // assignment op
+
+    // comparison op
+    // echo 'Equal (==) == > ' . $x == $y . "<br />";
+    // echo "Identical (===) == > " . $x === $y . "<br />";
+    // echo "Not Equal (!=) == > " . $x != $y . "<br />";
+    // echo "Not Equal (<>) == > " . $x <> $y . "<br />";
+    // echo "Not identical (!==) == > " . $x !== $y . "<br />";
+    // echo "Greater than (>) == > " . $x > $y . "<br />";
+    // echo "Less than (<) == > " . $x < $y . "<br />";
+    // echo "Greater than or equal (>=) == > " . $x >= $y . "<br />";
+    // echo "Less than or equal (<=) == > " . $x <= $y . "<br />";
+    // echo "Spaceship (<=>) == > " . $x <=> $y . "<br />";
+?>
---
+++ 07_controlStructure.php
@@ -0,0 +1,5 @@
+<?php
+    // Control structure means it gives the condition to the value
+
+    
+?>
